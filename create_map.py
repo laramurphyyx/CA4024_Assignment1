3041480e51e6503c2047dea7160a161f5e4660f5
murpl239 <lara.murphy239@mail.dcu.ie>
2022-03-25 23:53:46 +0000

    Reducing size of map from 100 to 50

--- create_map.py
+++ create_map.py
@@ -16,16 +16,16 @@ def create_random_road_map():
     horizontal_roads = []
     vertical_roads = []
 
-    horizontal_roads.append(random.randint(5, 95))
-    horizontal_road = random.randint(5, 95)
+    horizontal_roads.append(random.randint(5, 45))
+    horizontal_road = random.randint(5, 45)
     while horizontal_road - horizontal_roads[0] in range(-5,5):
-        horizontal_road = random.randint(5, 95)
+        horizontal_road = random.randint(5, 45)
     horizontal_roads.append(horizontal_road)
 
-    vertical_roads.append(random.randint(5, 95))
-    vertical_road = random.randint(5, 95)
+    vertical_roads.append(random.randint(5, 45))
+    vertical_road = random.randint(5, 45)
     while vertical_road - vertical_roads[0] in range(-5,5):
-        vertical_road = random.randint(5, 95)
+        vertical_road = random.randint(5, 45)
     vertical_roads.append(vertical_road)
 
     ###
@@ -35,23 +35,23 @@ def create_random_road_map():
     ### - Junctions are represented by a 3
     ###
 
-    road_map = np.zeros((100,100))
+    road_map = np.zeros((50,50))
 
     for x_coord in horizontal_roads:
-        for y_coord in range(0,100):
+        for y_coord in range(0,50):
             road_map[x_coord][y_coord] += 1
             road_map[x_coord + 1][y_coord] += 1
 
     for y_coord in vertical_roads:
-        for x_coord in range(0,100):
+        for x_coord in range(0,50):
             road_map[x_coord][y_coord] += 2
             road_map[x_coord][y_coord + 1] += 2
 
     # Adding top and bottom horizontal edge roads
     left_vertical_road = min(vertical_roads)
     right_vertical_road = max(vertical_roads) + 1
-    for x_coord in [0, 98]:
-        for y_coord in range(0,100):
+    for x_coord in [0, 48]:
+        for y_coord in range(0,50):
             if y_coord >= left_vertical_road and y_coord <= right_vertical_road:
                 road_map[x_coord][y_coord] += 1
                 road_map[x_coord + 1][y_coord] += 1
@@ -59,8 +59,8 @@ def create_random_road_map():
     # Adding left and right vertical edge roads
     top_horizontal_road = min(horizontal_roads)
     bottom_horizontal_road = max(horizontal_roads) + 1
-    for y_coord in [0, 98]:
-        for x_coord in range(0,100):
+    for y_coord in [0, 48]:
+        for x_coord in range(0,50):
             if x_coord >= top_horizontal_road and x_coord <= bottom_horizontal_road:
                 road_map[x_coord][y_coord] += 2
                 road_map[x_coord][y_coord + 1] += 2
