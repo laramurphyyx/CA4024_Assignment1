22f4f62b4b9f3e2619cdfb88810eb78f70349ce4
murpl239 <lara.murphy239@mail.dcu.ie>
2022-03-26 01:44:41 +0000

    Initialising several Drivers

--- ABM_traffic_simulator.py
+++ ABM_traffic_simulator.py
@@ -9,10 +9,13 @@ from functions import *
 ###
 
 number_drivers = 10
+drivers = []
+locations = []
 
-class driver:
+class Driver:
 
     def __init__(self, road_map):
+
         coordinates = initialise_driver_position(road_map)
         self.x = coordinates[0]
         self.y = coordinates[1]
@@ -21,12 +24,28 @@ class driver:
 def initialise():
 
     global road_map
+
+    ## Initialising a random road map
     road_map = create_random_road_map()
+
+    ## Initialising each driver
+    for i in range(0, number_drivers):
+        driver = Driver(road_map)
+
+        # Ensuring no two drivers start in the same location
+        while [driver.x, driver.y] in locations:
+            driver = Driver(road_map)
+
+        drivers.append(driver)
+        locations.append([driver.x, driver.y])
 
 def observe():
 
     plt.cla()
     plt.imshow(road_map)
+    drivers_x =  [driver.x for driver in drivers]
+    drivers_y =  [driver.y for driver in drivers]
+    plt.scatter(drivers_y, drivers_x)
     plt.show()
 
 def update():
