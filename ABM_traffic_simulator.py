ee8c02f8f8232c3f39c6d4a835ada1c1d521e959
murpl239 <lara.murphy239@mail.dcu.ie>
2022-03-26 17:29:40 +0000

    Allowing for direction options at junctions

--- ABM_traffic_simulator.py
+++ ABM_traffic_simulator.py
@@ -11,6 +11,18 @@ from functions import *
 number_drivers = 10
 drivers = []
 locations = []
+directions = {
+    'Right' : [0, 1],
+    'Left' : [0, -1],
+    'Up' : [-1, 0],
+    'Down' : [1, 0]
+}
+opposite_directions = {
+    'Right' : 'Left',
+    'Left' : 'Right',
+    'Up' : 'Down',
+    'Down' : 'Up'
+}
 
 class Driver:
 
@@ -20,13 +32,38 @@ class Driver:
         self.x = coordinates[0]
         self.y = coordinates[1]
         self.direction = initialise_driver_direction(self.x, self.y, road_map)
+        self.next_direction = self.direction
+        self.change_direction = False
+
+    def move_forward(self):
+        direction = directions[self.direction]
+        x_move = direction[0]
+        y_move = direction[1]
+
+        new_x = self.x + x_move
+        new_y = self.y + y_move
+
+        if road_map[new_x][new_y] != 3:
+            self.x = new_x
+            self.y = new_y
+
+        if new_x > 0 and new_x < 50 and new_y >0 and new_y < 50:
+            if road_map[new_x][new_y] == 3:
+                #print("New x is " + str(new_x))
+                #print("New y is " + str(new_y))
+                #print("Road map at [x, y] is " + str(road_map[new_x][new_y]))
+                print(find_junction_direction_options(road_map, new_x, new_y))
+
+        
 
 def initialise():
 
-    global road_map
+    global road_map, drivers, locations
 
-    ## Initialising a random road map
+    ## Initialising a random road map and lists of drivers and their locations
     road_map = create_random_road_map()
+    drivers = []
+    locations = []
 
     ## Initialising each driver
     for i in range(0, number_drivers):
@@ -45,11 +82,12 @@ def observe():
     plt.imshow(road_map)
     drivers_x =  [driver.x for driver in drivers]
     drivers_y =  [driver.y for driver in drivers]
-    plt.scatter(drivers_y, drivers_x)
+    plt.scatter(drivers_y, drivers_x, color="red")
     plt.show()
 
 def update():
-    pass
+    
+    for driver in drivers:
+        driver.move_forward()
 
-initialise()
-observe()
+pycxsimulator.GUI().start(func=[initialise, observe, update])
--- functions.py
+++ functions.py
@@ -136,5 +136,60 @@ def initialise_driver_direction(x, y, road_map):
         else:
             return "Up"
 
-
     return road_map[x][y]
+
+def find_junction_direction_options(road_map, x, y):
+
+    coordinates_of_junction = []
+
+    for x_coord in [x-1, x, x+1]:
+        if x_coord > 0 and x_coord < 50:
+            for y_coord in [y-1, y, y+1]:
+                if y_coord > 0 and y_coord < 50:
+                    if road_map[x_coord][y_coord] == 3:
+                        coordinates_of_junction.append([x_coord, y_coord])
+
+    # if x = 0 in any of them then 'up' is not an option
+    # if x = 49 in any of them then 'down' is not an option
+    # if y = 0 in any of them then 'left' is not an option
+    # if y = 49 in any of them then 'right' is not an option
+    for coordinate in coordinates_of_junction:
+        x = coordinate[0]
+        y = coordinate[1]
+
+        if x == 0:
+            # This is either junction on the top horizontal road
+            print("x is " + str(x))
+            for y_coord in [y-1, y-2]:
+                print("y_coord is " + str(y_coord))
+                print("road_map[x][y_coord] is " + str(road_map[x][y_coord]))
+                if road_map[x][y_coord] == 1:
+                    return ['Left', 'Down']
+                elif road_map[x][y_coord] == 0:
+                    return ['Right', 'Down']
+
+        elif x == 49:
+            # This is either junction on the bottom horizontal road
+            for y_coord in [y-1, y-2]:
+                if road_map[x][y_coord] == 1:
+                    return ['Left', 'Up']
+                elif road_map[x][y_coord] == 0:
+                    return ['Right', 'Up']
+
+        elif y == 0:
+            # This is either junction on the top vertical road
+            for x_coord in [x-1, x-2]:
+                if road_map[x_coord][y] == 2:
+                    return ['Right', 'Up']
+                elif road_map[x_coord][y] == 0:
+                    return ['Right', 'Down']
+
+        elif y == 49:
+            # This is either junction on the bottom vertical road
+            for x_coord in [x-1, x-2]:
+                if road_map[x_coord][y] == 2:
+                    return ['Left', 'Up']
+                elif road_map[x_coord][y] == 0:
+                    return ['Left', 'Down']
+    else:
+        return ['Right', 'Left', 'Up', 'Down']
